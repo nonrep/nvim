c98a8f1480a74d7ada1425d02a3fb00fda2802ca
nonrep <razvitie25@vkusvill.ru>
2025-05-29 20:53:00 +0300

    хз че добавил

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@
 require("config.lazy")
 
 vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,24 +1,44 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      if not opts.servers then
-        opts.servers = {}
-      end
-      opts.servers.gopls = {
-        settings = {
-          gopls = {
-            analyses = {
-              unusedparams = true,
-            },
-            staticcheck = true,
-            usePlaceholders = false,
-            completeUnimported = true, -- This enables auto-import
-            gofumpt = true,
-          },
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
         },
-      }
-      return opts
-    end,
-  },
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            -- Настройка gopls вручную
+            lspconfig.gopls.setup({
+                on_attach = function(client, bufnr)
+                    -- Отключаем signatureHelp (подсказки параметров)
+                    client.server_capabilities.signatureHelpProvider = nil
+
+                    -- Можно отключить и другие возможности
+                    -- client.server_capabilities.hoverProvider = false
+                end,
+                settings = {
+                    gopls = {
+                        -- Дополнительные настройки gopls
+                        analyses = {
+                            unusedparams = true,
+                        },
+                        staticcheck = true,
+                        completeUnimported = true,
+                        usePlaceholders = false,
+                    },
+                },
+            })
+
+            -- Минимальная настройка go.nvim
+            require("go").setup({
+                lsp_cfg = false, -- не перезаписывать нашу конфигурацию LSP
+                tag_options = "json=",
+            })
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", "gomod" },
+        build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    },
 }
